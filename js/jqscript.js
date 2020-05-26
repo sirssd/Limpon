caa86c96c69410a5bd19d5cb39094f9c8b30674c
sirssd <54711412+sirssd@users.noreply.github.com>
2020-05-26 12:05:18 +0500

    Закончил "Our Team"

---
+++ js/jqscript.js
@@ -0,0 +1,17 @@
+$(function() {
+    $('.btn-ricardo').click(function () {
+    $('.btn-ricardo').hide();
+    $('.social-ricardo').fadeIn();
+});
+
+$('.btn-alberto').click(function () {
+    $('.btn-alberto').hide();
+    $('.social-alberto').fadeIn();
+});
+
+$('.btn-alva').click(function () {
+    $('.btn-alva').hide();
+    $('.social-alva').fadeIn();
+});
+});
+
